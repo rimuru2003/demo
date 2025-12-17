916a8de91e8684d08bd19bc27e2082c27486f17c
Aryan Singh <karansnh007@gmail.com>
2025-12-17 16:51:40 +0530

    .

--- src/component/aboutus/About.jsx
+++ src/component/aboutus/About.jsx
@@ -95,7 +95,8 @@ const About = ({ goToTeam }) => {
   return (
     <div className="w-full h-screen text-black flex flex-col overflow-hidden">
       <KnowMoreCursor ref={cursorRef} />
-      <span className="bg-[#EAE4D8] w-36 text-center p-1  ml-20 text-2xl rounded-lg font-semibold">
+      <span className="bg-[#EAE4D8]  w-36 text-center p-1 mx-4 mb-4  xl:ml-20 text-2xl
+       rounded-lg font-semibold">
         <h1>About Us</h1>
       </span>
 
